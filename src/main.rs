e85810549bb1f1589a68d54b960ef184d6e342fc
ob2001 <butlero2001@outlook.com>
2023-07-18 00:26:37 -0400

    Allow user input for world dimensions and framerate

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,21 @@
 use game_of_life::startup::Cgol;
-use std::time::Duration;
+use std::{time::Duration, env};
 
 fn main() {
-    /*
-        TODO: Allow user to input custom world dimensions, frame rate, [? live cell character]
-    */
+    let args: Vec<String> = env::args().collect();
+    let (mut frame_delay, mut w, mut h) =  (Duration::from_millis(50), 40, 20);
 
-    // Create and run new GOL world
-    Cgol::new(Duration::from_millis(50), 175, 40).run();
+    
+    if args.contains(&String::from("dims")) {
+        let idx = args.iter().position(|r| r == "dims").unwrap();
+        (w, h) = (args[idx + 1].parse().unwrap(), args[idx + 2].parse().unwrap());
+    }
+
+    if args.contains(&String::from("delay")) {
+        let idx = args.iter().position(|r| r == "delay").unwrap();
+        frame_delay = Duration::from_millis(args[idx + 1].parse().unwrap());
+    }
+    
+    // Create and run new GOL world from given arguments (if any)
+    Cgol::new(frame_delay, w, h).run();
 }
